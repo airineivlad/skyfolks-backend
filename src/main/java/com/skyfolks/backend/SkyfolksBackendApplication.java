90629cb7bc719fcf340446a83541eb687c8b74ef
airineivlad <airinei17@gmail.com>
2020-10-03 12:47:15 +0100

    Initial project skeleton

---
+++ src/main/java/com/skyfolks/backend/SkyfolksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skyfolks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyfolksBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyfolksBackendApplication.class, args);
+    }
+
+}
